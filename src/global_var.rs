6e112364bd6d7ef49a0a20aa2ce7ea76e4f0c0b6
Lê Dũng <ledung.itsme@gmail.com>
2023-11-21 17:25:34 +0700

    refactor: refactor code

--- src/global_var.rs
+++ src/global_var.rs
@@ -2,31 +2,11 @@ use ext_php_rs::zend::ExecutorGlobals;
 
 /// Get a parameter from the $_GET array
 pub fn get(param: &str) -> String {
-    let value = ExecutorGlobals::get().symbol_table.get("_GET").and_then(|v| {
-        if v.is_array() {
-            let array = v.array();
-
-            if let Some(array) = array {
-                let result = array.get(&param);
-                if let Some(result) = result {
-                    if result.is_string() {
-                        result.string()
-                    } else {
-                        None
-                    }
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    });
-
-    match value {
-        Some(v) => v,
-        None => "".to_owned(),
-    }
+    ExecutorGlobals::get()
+        .symbol_table
+        .get("_GET")
+        .and_then(|v| v.array())
+        .and_then(|array| array.get(&param))
+        .and_then(|result| result.string())
+        .unwrap_or_else(|| "".to_owned())
 }
